98423cd22561eac4b8eb4f5ad4b3b91fa6daa563
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-02-10 12:11:16 +0200

    Minore README update

---
+++ test.rb
@@ -0,0 +1,30 @@
+module Maze
+  class Maze
+    attr_reader :rooms, :rows, :columns
+
+    def initialize(options)
+      @rooms = []
+      @rows = options[:rows]
+      @columns = options[:columns]
+
+      (1..rows).each_with_index do |row|
+        (1..columns).each_with_index do |column|
+          room = Room.new(x: column, y: row)
+          rooms << room
+        end
+      end
+    end
+
+    def find_room(position)
+      rooms.find { |room| room.position == position }
+    end
+
+    def all_rooms_visited?
+       rooms.all?(&:visited?)
+
+       rooms.
+
+
+    end
+  end
+end
